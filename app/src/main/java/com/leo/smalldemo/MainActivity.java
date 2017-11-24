406e558125de221ce52602e19b6a0888be035c76
LeXiaoWen <LeXiaoWen_Android@163.com>
2017-11-24 09:21:25 +0800

    重新编译插件

--- app/src/main/java/com/leo/smalldemo/MainActivity.java
+++ app/src/main/java/com/leo/smalldemo/MainActivity.java
@@ -1,7 +1,7 @@
 package com.leo.smalldemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import net.wequick.small.Small;
 
